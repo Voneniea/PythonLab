fb85da22e4291e24a1d411dbcdd6660363116e64
Voneniea <fanasis654@gmail.com>
2025-02-16 20:03:59 +0300

    задание 15-16

--- LR1/z15-19.py
+++ LR1/z15-19.py
@@ -1,15 +1,32 @@
+#Задача 5
 def is_global_minimum(arr, index):
     if not (0 <= index < len(arr)):
         raise ValueError("Индекс выходит за пределы массива")
 
     return arr[index] == min(arr)
 
+#Задача 17
+def swap_min_max(arr):
+    min_val = min(arr)
+    max_val = max(arr)
+    min_index = arr.index(min_val)
+    max_index = arr.index(max_val)
+    arr[min_index], arr[max_index] = arr[max_index], arr[min_index]
+    return arr
+
 
 arr = [3, 1, 4, 1, 5, 9, 2]
 print(f"Массив: {arr}")
-index = int(input("Введите индекс: "))
+choice = input("Выберите функцию: 1 - Проверить глобальный минимум, 2 - Поменять местами минимум и максимум: ")
 
-if is_global_minimum(arr, index):
-    print(f"Элемент {arr[index]} по индексу {index} является глобальным минимумом.")
+if choice == "1":
+    index = int(input("Введите индекс: "))
+    if is_global_minimum(arr, index):
+        print(f"Элемент {arr[index]} по индексу {index} является глобальным минимумом.")
+    else:
+        print(f"Элемент {arr[index]} по индексу {index} не является глобальным минимумом.")
+elif choice == "2":
+    arr = swap_min_max(arr)
+    print(f"Обновленный массив: {arr}")
 else:
-    print(f"Элемент {arr[index]} по индексу {index} не является глобальным минимумом.")
+    print("Некорректный выбор.")
